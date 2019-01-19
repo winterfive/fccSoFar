c637b3b5082a72bcb1b867e2a6f76876d35a17d3
Lee <winterfive@users.noreply.github.com>
2019-01-18 21:24:24 -0500

    Update test.js

--- test.js
+++ test.js
@@ -5,16 +5,26 @@ function getIndexToIns(arr, num) {
   arr.sort();
   
   for (var i = 0; i < arr.length; i++) {
+    // if sum and current value are the same
     if (num == arr[i]) {
-      break;
+      console.log(i);
+      value = i;
+      break;      
     }
     else if (num > arr[i] && num < arr[i + 1]) {
       value = i + 1;
+      break;      
+    }
+    else if (num > arr[arr.length - 1]) {
+      value = arr.length - 1;
+      break;
     }
   }
-
   console.log(value);
+
   return value;
+
+  
 }
 
-getIndexToIns([10, 20, 30, 40, 50], 35);
+getIndexToIns([3, 10, 5], 3);
