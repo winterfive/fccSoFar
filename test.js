93f898ce5a89bf60efa087f0cc0a3427c3a5f00f
Lee <winterfive@users.noreply.github.com>
2019-01-21 18:51:29 -0500

    Update: current challenge so far

--- test.js
+++ test.js
@@ -1,40 +1,21 @@
-function rot13(str) {
+function telephoneCheck(str) {
 
-  let arr = [];
-  let result = [];
-  let letCode, strLen = 0;
-  
-  // change all letters to uppercase, place in array
-  str = str.toUpperCase();
-  arr = str.split("");
+  // tests for 5555555555  
+  let regexOne = /1?[0-9]{10}/g;
 
-  strLen = arr.length;
+  // tests for 555-555-5555
+  let regexTwo = /1?[0-9]{3}-[0-9]{3}-[0-9]{4}/g;
 
-  // iterate through string
-  for (var x = 0; x < strLen; x++) {
-    if (/[a-z]/.test()) {
-      letCode = arr[x].charCodeAt();  
+  // tests for 1 555 555-5555
+  let regexThree = /1?\s([0-9]{3})\s[0-9]{3}-[0-9]{4}/g;
 
-      if ((letCode + 13) > 90) {
-        letCode = (letCode + 13) - 91 + 65;
-      } else {
-        letCode += 13;
-      }
-
-      // works to here
-      // get new letter and push it to result
-      let y = String.fromCharCode(letCode);
-      result.push(y);
-      console.log(y);      
-
-    } else {
-      result.push(arr[x]);
-    }
+  if(regexOne.test(str) || regexTwo.test(str) || regexThree.test(str)) {
+    return true;
+  } else {
+    return false;
   }
 
-  let a = result.join();
-  return a;
+
 }
 
-// Change the inputs below to test
-rot13("SERR PBQR PNZC");
+telephoneCheck("555-555-5555");
