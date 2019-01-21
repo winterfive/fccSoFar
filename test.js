60cb8cf392aecd946979eaa3e2693190fc479910
Lee <winterfive@users.noreply.github.com>
2019-01-21 12:47:23 -0500

    Added code created so far for next challenge

--- test.js
+++ test.js
@@ -1,43 +1,40 @@
-function mutation(arr) {
+function chunkArrayInGroups(arr, size) {
 
-  const searchers = arr[1];
-  const searchable = arr[0];
-  let result = true;
-  const x = [];
+  let result = [];
+  let subArr = [];  
+  let fullArraysNeeded = Math.floor(arr.length / size);  // the # of size-sized sub-arrays needed
+  let arrLen = arr.length;
+  let lastSubArrSize = arrLen % size; // size of last sub-array
 
-  // create array of letters from searchers
-  for (var j = 0; j < searchers.length; j++) {
-    x.push(searchers[j]);
-  }
-
-  //console.log(x); // good to here
-
-  // test each letter in searchable against each value in x
-  for (var i = 0; i < x.length; i++) {
-    if (searchable.indexOf(x[i]) < 0) {
-      result = false;
+  for (var i = 0; i < arrLen; i++) {
+    // fill up the full-size subArrays
+    if (result.length < fullArraysNeeded) {
+      if(subArr.length < size) {
+        subArr.push(arr[i]);
+      } else {
+        result.push(subArr);
+        subArr = [];
+      }
     }
+    // no more full-size subArrays needed
+    // create array of lastSubArrSize to handle remaining values in arr     
+    else {
+      // check if another subArray is needed
+      if (lastSubArrSize > 0) {
+        // create last subArray
+        for (var y = 0; y < lastSubArrSize; y++) {
+          subArr.push(arr[i]);
+        }
 
-    result = true;
+        result.push(subArr);
+        subArr = [];
+      }      
+    }
   }
-
-  return result;
-
-/*
-  // use a regex test
-  const searchTerm = arr[1];
-  const searchable = arr[0];
-  let regex = RegExp(/[searchTerm]/ig);
-
-  let result = regex.test(searchable)
-
-  // since each letter, need to use iteration?
-  // break string into seperate letter array?
-  console.log(result);
   return result;
 }
-*/
 
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
 /*
 function mutation(arr) {
 
