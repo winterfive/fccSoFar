733e197e3a33dad826b7704a48c2c79274679e72
Lee <winterfive@users.noreply.github.com>
2019-01-21 13:42:02 -0500

    Completed solution for challenge

--- test.js
+++ test.js
@@ -11,27 +11,33 @@ function chunkArrayInGroups(arr, size) {
     if (result.length < fullArraysNeeded) {
       if(subArr.length < size) {
         subArr.push(arr[i]);
-      } else {
+      } 
+      
+      if (subArr.length == size) {
         result.push(subArr);
         subArr = [];
       }
     }
+
     // no more full-size subArrays needed
     // create array of lastSubArrSize to handle remaining values in arr     
     else {
       // check if another subArray is needed
       if (lastSubArrSize > 0) {
-        // create last subArray
-        for (var y = 0; y < lastSubArrSize; y++) {
           subArr.push(arr[i]);
-        }
-
+          lastSubArrSize--;    
+      }
+      if (lastSubArrSize == 0) {
+        if (subArr != NaN)
         result.push(subArr);
-        subArr = [];
-      }      
+        subArr = [];   
+      }
+         
     }
   }
+  
+  console.log("result is: " + result);
   return result;
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
