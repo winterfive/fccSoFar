c859f8c20a65dbb870df2a0f2f27d13853aa0209
Lee <winterfive@users.noreply.github.com>
2019-01-21 19:14:11 -0500

    Updating FCC challenge code

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 function telephoneCheck(str) {
 
-  // tests for 5555555555  
+  // tests for 10 digits  
   let regexOne = /1?[0-9]{10}/g;
 
-  // tests for 555-555-5555
-  let regexTwo = /1?[0-9]{3}-[0-9]{3}-[0-9]{4}/g;
+  // tests for 555-555-5555, 555 555 5555
+  let regexTwo = /1?\s?[0-9]{3}-?\s?[0-9]{3}-?\s?[0-9]{4}/g;
 
-  // tests for 1 555 555-5555
-  let regexThree = /1?\s([0-9]{3})\s[0-9]{3}-[0-9]{4}/g;
+  // tests for 1 (555) 555-5555
+  let regexThree = /1?\s?([0-9]{3})\s?[0-9]{3}-?[0-9]{4}/g;
 
   if(regexOne.test(str) || regexTwo.test(str) || regexThree.test(str)) {
     return true;
