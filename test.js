95b72fd9ba496dbe1597d68ee05511ddf63f3582
Lee <winterfive@users.noreply.github.com>
2019-01-21 20:42:41 -0500

    Added another regex

--- test.js
+++ test.js
@@ -1,15 +1,27 @@
 function telephoneCheck(str) {
 
   // tests for 10 digits  
-  let regexOne = /1?[0-9]{10}/g;
+  let regexOne = /1?[0-9]{10, 10}/g;
 
-  let regexTwo = /1?\s?([0-9]{3})-?\s?([0-9]{3})-?\s?([0-9]{4})/g;
+  let regexTwo = /1?\s?([0-9]{3,3})-?\s?([0-9]{3,3})-?\s?([0-9]{4,4})/g;
 
-  let regexThree = /1?\s?\(([0-9]{3})\)\s?([0-9]{3})-?([0-9]{4})/g;
+  let regexThree = /1?\s?\(([0-9]{3,3})\)\s?([0-9]{3,3})-?([0-9]{4,4})/g;
 
-  if(regexOne.test(str) || regexTwo.test(str) || regexThree.test(str)) {
+  let regexFour = /1?\(?\s?([0-9]{3,3})-?\)?\s?([0-9]{3,3})-?\s?([0-9]{4,4})/g;
+
+  if(regexOne.test(str)) {
     return true;
-  } else {
+  } 
+  else if(regexTwo.test(str)) {
+    return true;
+  }
+  else if(regexThree.test(str)) {
+    return true;
+  }
+  else if(regexFour.test(str)) {
+    return true;
+  }  
+  else {
     return false;
   }
 
