fa93f1c8de812b163903c2af2c2c34df06e67832
Lee <winterfive@users.noreply.github.com>
2019-01-21 19:30:31 -0500

    Removed third regex

--- test.js
+++ test.js
@@ -3,13 +3,9 @@ function telephoneCheck(str) {
   // tests for 10 digits  
   let regexOne = /1?[0-9]{10}/g;
 
-  // tests for 555-555-5555, 555 555 5555
-  let regexTwo = /1?\s?[0-9]{3}-?\s?[0-9]{3}-?\s?[0-9]{4}/g;
+  let regexTwo = /1?\s?([0-9]{3})-?\s?([0-9]{3})-?\s?([0-9]{4})/g;
 
-  // tests for 1 (555) 555-5555
-  let regexThree = /1?\s?([0-9]{3})\s?[0-9]{3}-?[0-9]{4}/g;
-
-  if(regexOne.test(str) || regexTwo.test(str) || regexThree.test(str)) {
+  if(regexOne.test(str) || regexTwo.test(str)) {
     return true;
   } else {
     return false;
@@ -18,4 +14,4 @@ function telephoneCheck(str) {
 
 }
 
-telephoneCheck("555-555-5555");
+telephoneCheck("1(555)555-5555");
