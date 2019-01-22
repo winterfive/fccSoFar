0f94dcf48ae26b4797590a90b2a8d74424d0ae70
Lee <winterfive@users.noreply.github.com>
2019-01-22 12:19:33 -0500

    Updated: catches all but 3 cases

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 function telephoneCheck(str) {
 
    
-  let regexOne = /[0-9]{10,10}/g;
-  let regexTwo = /1?\s?([0-9]{3,3})-?([0-9]{3,3})-?([0-9]{4,4})/g; // -
+  let regexOne = /^[0-9]{10,10}/g;
+  let regexTwo = /^1?\s?([0-9]{3,3})-?([0-9]{3,3})-?([0-9]{4,4})/g; // -
   let regexThree = /^1?\s?\([0-9]{3,3}\)/g;
-  let regexFour = /1?\s[0-9]{3,3}\s[0-9]{3,3}\s[0-9]{4,4}/g;
+  let regexFour = /^1?\s[0-9]{3,3}\s[0-9]{3,3}\s[0-9]{4,4}/g;
 
   if(regexOne.test(str)) {
     return true;
