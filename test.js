645d3855b4e01b548f16cefb725cb1379b0110b0
Lee <winterfive@users.noreply.github.com>
2019-01-22 12:54:30 -0500

    Code updated, now catches all but 2 cases

--- test.js
+++ test.js
@@ -1,13 +1,23 @@
 function telephoneCheck(str) {
 
    
-  let regexOne = /^[0-9]{10,10}/g;
-  let regexTwo = /^1?\s?([0-9]{3,3})-?([0-9]{3,3})-?([0-9]{4,4})/g; // -
-  let regexThree = /^1?\s?\([0-9]{3,3}\)/g;
-  let regexFour = /^1?\s[0-9]{3,3}\s[0-9]{3,3}\s[0-9]{4,4}/g;
+  let regexOne = /^[0-9]{10}/g;
+  let regexTwo = /^1?\s?([0-9]{3})-?([0-9]{3})-?([0-9]{4})/g; // -
+  let regexThree = /^1?\s?\([0-9]{3}\)/g;
+  let regexFour = /^1?\s[0-9]{3}\s[0-9]{3}\s[0-9]{4}/g;
+  let regexFive = /\?+/g;
+  let regexSix = /[0-9]{11}/g;
 
   if(regexOne.test(str)) {
     return true;
+  }
+
+  else if(regexSix.test(str)) {
+    return false;
+  }
+
+  else if(str.includes("?")) {
+    return false;
   }
   
   else if(regexTwo.test(str)) {
@@ -20,13 +30,17 @@ function telephoneCheck(str) {
   
   else if(regexFour.test(str)) {
     return true;
+  } 
+
+  else if(regexFive.test(str)) {
+    return true;
   }  
+
+  
   
   else {
     return false;
   }
-
-
 }
 
 telephoneCheck("1(555)555-5555");
