d078fb77dc2434ac41823fac7f249e3fee217383
Lee <winterfive@users.noreply.github.com>
2019-01-22 12:10:39 -0500

    Update to regex challenge

--- test.js
+++ test.js
@@ -1,26 +1,26 @@
 function telephoneCheck(str) {
 
-  // tests for 10 digits  
-  let regexOne = /1?[0-9]{10, 10}/g;
-
-  let regexTwo = /1?\s?([0-9]{3,3})-?\s?([0-9]{3,3})-?\s?([0-9]{4,4})/g;
-
-  let regexThree = /1?\s?\(([0-9]{3,3})\)\s?([0-9]{3,3})-?([0-9]{4,4})/g;
-
-  let regexFour = /1?\(?\s?([0-9]{3,3})-?\)?\s?([0-9]{3,3})-?\s?([0-9]{4,4})/g;
+   
+  let regexOne = /[0-9]{10,10}/g;
+  let regexTwo = /1?\s?([0-9]{3,3})-?([0-9]{3,3})-?([0-9]{4,4})/g; // -
+  let regexThree = /^1?\s?\([0-9]{3,3}\)/g;
 
   if(regexOne.test(str)) {
     return true;
-  } 
+  }
+  
   else if(regexTwo.test(str)) {
     return true;
   }
+  
   else if(regexThree.test(str)) {
     return true;
   }
+  /*
   else if(regexFour.test(str)) {
     return true;
   }  
+  */
   else {
     return false;
   }
