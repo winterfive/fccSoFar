b119053deae53a23725436ac91940a10b00e45f4
Lee <winterfive@users.noreply.github.com>
2019-01-23 18:23:04 -0500

    Added code from another challenge

--- test.js
+++ test.js
@@ -9,58 +9,24 @@ function telephoneCheck(str) {
 }
 
 telephoneCheck("555-555-5555");
-/*
-function telephoneCheck(str) {
-   
-  let regexOne = /^[0-9]{10, 10}/g;
-  let regexTwo = /^1?\s?([0-9]{3})-?([0-9]{3})-?([0-9]{4})/g; // -
-  let regexThree = /^1?\s?\([0-9]{3}\)/g;
-  let regexFour = /^1?\s[0-9]{3}\s[0-9]{3}\s[0-9]{4}/g;
-  let regexFive = /\?+/g;
-  let regexSix = /[0-9]{11}/g;
 
-  if(regexOne.test(str)) {
-    console.log("found 1");
-    return true;
-  }
-  
-  else if (str.charAt(str.length - 1) == "2") {
-    return false;
-  }
-  
-  else if(regexSix.test(str)) {
-    console.log("found 6");
-    return false;
+var averageRating = 0;
+let num = 0;
+let result;
+
+watchList.reduce(function(sum, current){
+  if(current.Director === "Christopher Nolan") {
+    sum += parseFloat(current.imdbRating);
+    num++;
   }
 
-  else if(str.includes("?")) {
-    console.log("found ?");
-    return false;
-  }
-  
-  else if(regexTwo.test(str)) {
-    console.log("found 2");
-    return true;
-  }
-  
-  else if(regexThree.test(str)) {
-    console.log("found 3");
-    return true;
-  }
-  
-  else if(regexFour.test(str)) {
-    console.log("found 4");
-    return true;
-  } 
+  console.log(num);
 
-  else if(regexFive.test(str)) {
-    console.log("found 5");
-    return true;
-  }    
-  else {
-    return false;
-  }
-}
+  return sum/num;
+});
 
-telephoneCheck("27576227382");
-*/
+
+
+// Add your code above this line
+
+console.log(averageRating); 
