e86334cc676f4427bc6b2f4d515dfc33f119d8ec
Lee <winterfive@users.noreply.github.com>
2019-01-28 12:58:17 -0500

    Added code sofar for Everything Be True challenge

--- test.js
+++ test.js
@@ -466,3 +466,23 @@ function binaryAgent(str) {
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+// Everything Be True
+
+function truthCheck(collection, pre) {
+  
+  let newArr = [].concat(...collection);
+
+  let bool = newArr.every(function(obj) {
+    if(obj.hasOwnProperty(pre)) {
+      if(obj.pre) {
+        return true;
+      }
+    }
+  });
+
+  console.log(bool);
+  return bool;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
