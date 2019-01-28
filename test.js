a153182fe5c393d32c0f0047c2706c27b5d7932e
Lee <winterfive@users.noreply.github.com>
2019-01-28 12:23:51 -0500

    Binary Agent: Added working code

--- test.js
+++ test.js
@@ -434,3 +434,35 @@ function steamrollArray(arr) {
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
+
+// Binary Agent
+
+function binaryAgent(str) {
+
+  let numArr = str.split(" ");  
+  let decArr = [];
+  let finalArr = [];
+
+  // Chagne binary to decimal
+  numArr.forEach(function(binary) {
+    let newNum = 0;
+    let binaryArr = [128, 64, 32, 16, 8, 4, 2, 1];
+
+    for(let i = 0; i < binary.length; i++) {
+      if(binary[i] == 1) {
+        newNum += binaryArr[i];
+      }
+    }
+    decArr.push(newNum);
+  });
+
+  decArr.forEach(function(decimal) {
+    let letter = String.fromCharCode(decimal);
+    finalArr.push(letter);
+  });
+
+  let result = finalArr.join("");
+  return result;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
