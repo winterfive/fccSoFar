049b432e84a53f0c0b1d944ab22ade3ef8cb98c0
Lee <winterfive@users.noreply.github.com>
2019-01-29 16:56:12 -0500

    Added code sofar for cash Register Challenge

--- test.js
+++ test.js
@@ -590,3 +590,17 @@ function orbitalPeriod(arr) {
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+// Cash Register
+
+function checkCashRegister(price, cash, cid) {
+
+  let drawer = [];
+  
+  cid.forEach(function(item) {
+    let smallArr = [item[0], item[1]];
+    drawer.push(smallArr);
+  });
+}
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
